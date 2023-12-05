0f12a5234fb3f682e68bed4e83602988df3bb128
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-05 13:09:59 -0800

    Update Steps-in-primes.js

--- Steps-in-primes.js
+++ Steps-in-primes.js
@@ -51,3 +51,31 @@ function step(g, m, n) {
 }
 
 // or
+
+function prime(n) {
+  if (n === 2) {
+    return true;
+  } else if ((n < 2) || (n % 2 === 0)) {
+    return false;
+  } else {
+    for (let i = 3; i <= Math.sqrt(n); i += 2) {
+      if (n % i === 0)
+        return false;
+    }
+    return true;
+  }
+}
+
+function step(g, m, n) {
+  let res = [];
+  let i = m;
+  while (i <= n - g) {
+    if (prime(i) && prime(i + g)) {
+      res.push(i);
+      res.push(i + g);
+      return res
+    }
+    i++;
+  }
+  return null;
+}

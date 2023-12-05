d1873c621c0348acc70621b0ff5322cce4124c07
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-05 12:40:25 -0800

    Update Steps-in-primes.js

--- Steps-in-primes.js
+++ Steps-in-primes.js
@@ -34,3 +34,18 @@ Notes:
 
 
 // Solution
+
+function isPrime(n) {
+  let r = Math.sqrt(n) | 0;
+  for (let i = 2; i <= r && n % i; i++);
+  return i > r;
+}
+
+function step(g, m, n) {
+  for (let i = m; i <= n - g; i++) {
+    if (isPrime(i) && isPrime(i + g)) {
+      return [i, i + g];
+    }
+  }
+  return null;
+}
